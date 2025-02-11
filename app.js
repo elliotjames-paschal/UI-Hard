c367da814474d49849fc008dcd9d4f5dc77c5d8a
Elliot <elliotjpaschal@gmail.com>
2025-02-11 00:24:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -55,3 +55,12 @@ function notify(message) {
   document.getElementById('notification').innerText = message;
 }
 
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
